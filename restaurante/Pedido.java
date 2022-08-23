b95546f410b6fe7cecb7009eb4eb8728430c6873
Edwardo394 <69097770+Edwardo394@users.noreply.github.com>
2022-08-23 09:13:28 -0300

    Add files via upload

---
+++ restaurante/Pedido.java
@@ -0,0 +1,9 @@
+public class Pedido{ 
+    private String tipo;
+    private int tempoNecessarioAoProcessamento;
+    private int tempoGastoEmProcessamento;
+
+    public void imprime(){
+        System.out.println("Método imprime()");
+    }
+}
---
+++ restaurante/UnidadeDeProcessamento.java
@@ -0,0 +1,31 @@
+public class UnidadeDeProcessamento
+//Tem que ter uma fila;
+// X unidades de armazenamento interno nessa classe;
+
+{
+    public void ColocaNaFilaDeProcessamento(Pedido P){
+
+    }
+
+    public Pedido RetiraPedidoDaFilaDeProcessamento(){
+        
+    }
+
+    public boolean temUnidadeDeProcessaentoLivre(){
+
+    }
+
+    public void AtribuiPedidoAoProcessadorLivre(Pedido P)
+    {
+
+    }
+
+    public boolean temProcessamentoFinalizado(int t){
+
+    }
+
+    public Pedido ConcluiProcessamento(){
+
+    }
+
+}
