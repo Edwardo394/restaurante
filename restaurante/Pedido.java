a055456db5c4f8a0fd78cae4408cdd7c5f55dc30
Edwardo394 <69097770+Edwardo394@users.noreply.github.com>
2022-08-25 22:28:31 -0300

    Delete restaurante directory

--- restaurante/Pedido.java
+++
@@ -1,9 +0,0 @@
-public class Pedido{ 
-    private String tipo;
-    private int tempoNecessarioAoProcessamento;
-    private int tempoGastoEmProcessamento;
-
-    public void imprime(){
-        System.out.println("Método imprime()");
-    }
-}
